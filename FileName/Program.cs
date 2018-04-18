6ffe22c9e0d0b3ebbcea4dab5c425a8281ea77b2
Zhang Zexiao <zhangzexiaozhang@163.com>
2018-04-18 16:31:13 +0800

    Add files via upload

---
+++ FileName/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+namespace FileName
+{
+	class Program
+	{
+		[System.STAThread]
+		static void Main(string[]args)
+		{
+			System.Windows.Clipboard.SetText(System.IO.Path.GetInvalidFileNameChars().Aggregate(args[0],(fileName,invalidChar)=>fileName.Replace(invalidChar,'-')));
+		}
+	}
+}
